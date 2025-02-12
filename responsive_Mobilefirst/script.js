29fd523ad51018b24b2c8a603eb8af86d485c5be
Paula Sosa Hernandez <paulasosahdez@gmail.com>
2025-02-12 00:53:49 -0500

    update scrollreveal

--- responsive_Mobilefirst/script.js
+++ responsive_Mobilefirst/script.js
@@ -7,3 +7,9 @@ ScrollReveal().reveal('.content', {
     interval: 100           // Aplica intervalo 100 es para que cargue uno por uno 
 });
 
+ScrollReveal().reveal('.icons', {
+    delay: 150,             //Controla la velocidad en que aparecen en pantalla
+    duration: 150,          //Controla la velocidad en que aparecen en pantalla
+    reset: false,            //Si quiero que cargue siempre suba o baje se deja 'true'
+    interval: 100           // Aplica intervalo 100 es para que cargue uno por uno 
+});
