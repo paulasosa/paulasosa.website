b41e5b8621530550f21cce1c239127c32cd89782
Paula Sosa Hernandez <paulasosahdez@gmail.com>
2025-02-11 22:08:21 -0500

    guardado de proyecto sin responsive y publicacion de proyecto con responsive

---
+++ responsive_Mobilefirst/script.js
@@ -0,0 +1,9 @@
+// console.log("Hola Paulita")
+
+ScrollReveal().reveal('.content', {
+    delay: 150,             //Controla la velocidad en que aparecen en pantalla
+    duration: 150,          //Controla la velocidad en que aparecen en pantalla
+    reset: false,            //Si quiero que cargue siempre suba o baje se deja 'true'
+    interval: 100           // Aplica intervalo 100 es para que cargue uno por uno 
+});
+
